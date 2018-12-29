4cb72c0d5de983e970f21925e0d77db27eadfc29
Sourire <a12wang@126.com>
2018-12-29 13:55:39 +0800

    '14'

--- src/api/api.js
+++ src/api/api.js
@@ -2,48 +2,48 @@ import cookie from 'react-cookies';
 import { get, post } from './request.js';
 
 const displaypig = () => {
-  return get('http://172.16.1.167:8080/getAllPig').then((res) => {
+  return get('http://172.16.1.170:8080/getAllPig').then((res) => {
     return res;
   });
 };
 
 const addPig = (value) => {
-  return post('http://172.16.1.167:8080/addpig', {
+  return post('http://172.16.1.170:8080/addpig', {
     data: value,
   });
 };
 
 const checkSelect = () => {
-  return get('http://172.16.1.167:8080/getAllPig');
+  return get('http://172.16.1.170:8080/getAllPig');
 };
 // 显示猪的详情
 const showDetail = (id) => {
-  return get(`http://172.16.1.167:8080/getPigInfo/${id}`);
+  return get(`http://172.16.1.170:8080/getPigInfo/${id}`);
 };
 // 显示健康信息
 const showHealthMin = (id) => {
-  return get(`http://172.16.1.167:8080/getPigHealthInfo/${id}`).then((resp) => {
+  return get(`http://172.16.1.170:8080/getPigHealthInfo/${id}`).then((resp) => {
     return resp;
   });
 };
 
 
 const showpiglist = (id) => {
-  return get(`http://172.16.1.167:8080/getPigList/${id}`);
+  return get(`http://172.16.1.170:8080/getPigList/${id}`);
 };
 // 显示猪舍信息
 const pigsty = () => {
-  return get('http://172.16.1.167:8080/pigHouseList');
+  return get('http://172.16.1.170:8080/pigHouseList');
 };
 
 const showEnvironmentalMin = (id) => {
-  return get(`http://172.16.1.167:8080/getPigHouseEnv/${id}`).then((resp) => {
+  return get(`http://172.16.1.170:8080/getPigHouseEnv/${id}`).then((resp) => {
     return resp;
   });
 };
 // 添加猪舍信息
 const addPigsty = (value) => {
-  return post('http://172.16.1.167:8080/addPighouse', {
+  return post('http://172.16.1.170:8080/addPighouse', {
     data: value,
   });
 };
@@ -51,7 +51,7 @@ const addPigsty = (value) => {
 // 显示猪舍列表
 
 const pigstylist = () => {
-  return get('http://172.16.1.167:8080/pigHouseIdList').then((resp) => {
+  return get('http://172.16.1.170:8080/pigHouseIdList').then((resp) => {
     return resp;
   });
 };
